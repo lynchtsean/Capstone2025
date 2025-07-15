2dbee58dd86936d3dddc556a69bab7556857b27a
lynchtsean <lynchtsean@gmail.com>
2025-07-15 19:51:36 -0400

    Update lambda.tf

--- guitar-chatbot-terraform/lambda.tf
+++ guitar-chatbot-terraform/lambda.tf
@@ -1,1 +1,35 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/chatbotlogic.py"
+  output_path = "${path.module}/chatbotlogic.zip"
+}
+
+resource "aws_lambda_function" "chatbot" {
+  function_name = "ChatbotLogic"
+  handler       = "chatbotlogic.lambda_handler"
+  runtime       = "python3.11"
+  role          = aws_iam_role.lambda_exec.arn
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  timeout       = 10
+}
