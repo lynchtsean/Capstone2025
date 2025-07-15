fd10d0498c9f3331e7d5c9c5e8624c830fdfb06e
lynchtsean <lynchtsean@gmail.com>
2025-07-15 19:52:48 -0400

    Update api_gateway.tf

--- guitar-chatbot-terraform/api_gateway.tf
+++ guitar-chatbot-terraform/api_gateway.tf
@@ -1,1 +1,40 @@
+resource "aws_api_gateway_rest_api" "chatbot_api" {
+  name        = "GuitarChatbotAPI"
+  description = "API Gateway for Guitar Chatbot"
+}
 
+resource "aws_api_gateway_resource" "chatbot_resource" {
+  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
+  parent_id   = aws_api_gateway_rest_api.chatbot_api.root_resource_id
+  path_part   = "ChatbotLogic"
+}
+
+resource "aws_api_gateway_method" "post_method" {
+  rest_api_id   = aws_api_gateway_rest_api.chatbot_api.id
+  resource_id   = aws_api_gateway_resource.chatbot_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.chatbot_api.id
+  resource_id             = aws_api_gateway_resource.chatbot_resource.id
+  http_method             = aws_api_gateway_method.post_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.chatbot.invoke_arn
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.chatbot.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.chatbot_api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = [aws_api_gateway_integration.lambda_integration]
+  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
+  stage_name  = "prod"
+}
