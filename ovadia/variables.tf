b451a71585f34943a06f985de35b9cdf74db6f63
ovadiabigio <145715771+ovadiabigio@users.noreply.github.com>
2025-07-21 18:32:48 -0400

    Create variables.tf

---
+++ ovadia/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+variable "s3_bucket" {
+  default = "mood-generator-lambda"
+}
+
+variable "lambda_function_name" {
+  default = "moodGeneratorLambda"
+}
